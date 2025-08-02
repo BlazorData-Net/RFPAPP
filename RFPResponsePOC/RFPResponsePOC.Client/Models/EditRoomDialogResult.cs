9f808cf30b1455037c881462df6e0f1274958529
Michael Washington <webmaster@adefwebserver.com>
2025-08-02 05:40:00 -0700

    Return structured result from room dialog

---
+++ RFPResponsePOC/RFPResponsePOC.Client/Models/EditRoomDialogResult.cs
@@ -0,0 +1,10 @@
+using RFPResponsePOC.Models;
+
+namespace RFPResponsePOC.Client.Models
+{
+    public class EditRoomDialogResult
+    {
+        public bool IsDeleted { get; set; }
+        public Room Room { get; set; }
+    }
+}
