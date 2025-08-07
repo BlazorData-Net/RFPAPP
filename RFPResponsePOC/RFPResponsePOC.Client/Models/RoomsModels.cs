4ffb5c8f68bb049b51192bee69bf3758e485a257
Michael Washington <webmaster@adefwebserver.com>
2025-08-07 13:35:21 -0700

    Adding upload for proposals

---
+++ RFPResponsePOC/RFPResponsePOC.Client/Models/RoomsModels.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+
+namespace RFPResponsePOC.Client.Models
+{
+    public class RoomsRequest
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("start_date")]
+        public DateTime StartDate { get; set; }
+
+        [JsonProperty("start_time")]
+        public TimeSpan StartTime { get; set; }
+
+        [JsonProperty("end_date")]
+        public DateTime EndDate { get; set; }
+
+        [JsonProperty("end_time")]
+        public TimeSpan EndTime { get; set; }
+
+        [JsonProperty("room_type")]
+        public string RoomType { get; set; }
+
+        [JsonProperty("attendance")]
+        public int Attendance { get; set; }
+
+        [JsonProperty("notes")]
+        public string Notes { get; set; }
+    }
+
+    public class RoomsResponse
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("start_date")]
+        public DateTime StartDate { get; set; }
+
+        [JsonProperty("start_time")]
+        public TimeSpan StartTime { get; set; }
+
+        [JsonProperty("end_date")]
+        public DateTime EndDate { get; set; }
+
+        [JsonProperty("end_time")]
+        public TimeSpan EndTime { get; set; }
+
+        [JsonProperty("room_type")]
+        public string RoomType { get; set; }
+
+        [JsonProperty("attendance")]
+        public int Attendance { get; set; }
+
+        [JsonProperty("notes")]
+        public string Notes { get; set; }
+    }
+}
