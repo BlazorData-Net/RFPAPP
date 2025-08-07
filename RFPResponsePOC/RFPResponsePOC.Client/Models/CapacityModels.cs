77c2fe02980c77374132c467bc3908c3a31b7c2e
Michael Washington <webmaster@adefwebserver.com>
2025-08-07 06:23:24 -0700

    Added roomsRequest and RoomsResponse

--- RFPResponsePOC/RFPResponsePOC.Client/Models/CapacityModels.cs
+++ RFPResponsePOC/RFPResponsePOC.Client/Models/CapacityModels.cs
@@ -37,4 +37,58 @@ namespace RFPResponsePOC.Models
         [JsonProperty("boardroom")] public int? Boardroom { get; set; }
         [JsonProperty("crescentRounds")] public int? CrescentRounds { get; set; }
     }
+
+    public class RoomsRequest
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("start_date")]
+        public DateTime StartDate { get; set; }
+
+        [JsonProperty("start_time")]
+        public TimeSpan StartTime { get; set; }
+
+        [JsonProperty("end_date")]
+        public DateTime EndDate { get; set; }
+
+        [JsonProperty("end_time")]
+        public TimeSpan EndTime { get; set; }
+
+        [JsonProperty("room_type")]
+        public string RoomType { get; set; }
+
+        [JsonProperty("attendance")]
+        public int Attendance { get; set; }
+
+        [JsonProperty("notes")]
+        public string Notes { get; set; }
+    }
+
+    public class RoomsResponse
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("start_date")]
+        public DateTime StartDate { get; set; }
+
+        [JsonProperty("start_time")]
+        public TimeSpan StartTime { get; set; }
+
+        [JsonProperty("end_date")]
+        public DateTime EndDate { get; set; }
+
+        [JsonProperty("end_time")]
+        public TimeSpan EndTime { get; set; }
+
+        [JsonProperty("room_type")]
+        public string RoomType { get; set; }
+
+        [JsonProperty("attendance")]
+        public int Attendance { get; set; }
+
+        [JsonProperty("notes")]
+        public string Notes { get; set; }
+    }
 }
